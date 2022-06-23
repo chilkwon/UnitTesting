797f5fe57f18b37b123405d9bf87fc050914181e
Chil's Coding World <mbsrkch@gmail.com>
2022-06-22 23:32:33 -0400

    Testing code with parameter values

---
+++ calculator/src/main/java/com/chil/maven/calculator/calculator/Calculator.java
@@ -0,0 +1,6 @@
+package com.chil.maven.calculator.calculator;
+
+public interface Calculator {
+	int add(int num1, int num2);
+	
+}
---
+++ calculator/src/main/java/com/chil/maven/calculator/calculator/CalculatorImpl.java
@@ -0,0 +1,11 @@
+package com.chil.maven.calculator.calculator;
+
+public class CalculatorImpl implements Calculator {
+
+
+	public int add(int num1, int num2) {
+		// TODO Auto-generated method stub
+		return num1+num2;
+	}
+
+}
